ba985b05b46c2eded8d4dde20a12796c4faea8d9
Fred_Dead_Redemption <frederikdau@gmail.com>
2023-10-11 23:26:50 +0200

    jeg er den bedste

--- src/Chessboard.java
+++ src/Chessboard.java
@@ -1,13 +1,27 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Chessboard {
 
     JPanel panel;
-    public void init(){
+    public void init() throws IOException {
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 526, 549);
         frame.setTitle("Skak");
+
+        BufferedImage all= ImageIO.read(new File("src/chess.png"));
+        Image[] imgs =new Image[12];
+        int ind=0;
+        for(int y=0;y<400;y+=200){
+            for(int x=0;x<1200;x+=200){
+                imgs[ind]=all.getSubimage(x, y, 200, 200).getScaledInstance(64, 64, BufferedImage.SCALE_SMOOTH);
+                ind++;
+            }
+        }
         panel = new JPanel() {
             @Override
             public void paint(Graphics g) {
@@ -25,6 +39,19 @@ public class Chessboard {
                     }
                     lightSquare = !lightSquare;
                 }
+                for (int i = 0; i < Piece.arrayOfPieces.length; i++) {
+                    Piece p = Piece.arrayOfPieces[i];
+                    int ind = switch (p.type) {
+                        case "q", "Q" -> 1;
+                        case "b", "B" -> 2;
+                        case "n", "N" -> 3;
+                        case "r", "R" -> 4;
+                        case "p", "P" -> 5;
+                        default -> 0;
+                    };
+                    if(p.isWhite()) { ind += 6;}
+                    g.drawImage(imgs[ind], p.position * 64, p.position * 64, this);
+                }
             }
         };
         frame.add(panel);
@@ -35,4 +62,6 @@ public class Chessboard {
     public void animate(){
         panel.repaint();
     }
+
+    public static {}[] animationGrid[]
 }
--- src/Piece.java
+++ src/Piece.java
@@ -1,13 +1,57 @@
+import java.util.Objects;
+
 public class Piece {
-    int position; // 0-63
+    int position; // 0-63 bitboard
     String type; // P="white pawn" p="black pawn" N="white knight" k="black king"...
-    boolean hasMoved;
-    boolean hasBeenSlaughtered;
+    boolean hasMoved; // for castle & double pawn moves
+    boolean hasBeenSlaughtered; // for castle
     public Piece(int position, String type){
         this.position = position;
         this.type = type;
         this.hasMoved = false;
         this.hasBeenSlaughtered = false;
     }
+
+    public Boolean isWhite(){
+        return this.type.matches("K|Q|B|N|R|P");
+    }
+
+    public static Piece[] arrayOfPieces = {
+            // 0-15 BLACK
+            new Piece(56, "r"),
+            new Piece(57, "n"),
+            new Piece(58, "b"),
+            new Piece(59, "q"),
+            new Piece(60, "k"),
+            new Piece(61, "b"),
+            new Piece(62, "n"),
+            new Piece(63, "r"),
+            new Piece(48, "p"),
+            new Piece(49, "p"),
+            new Piece(50, "p"),
+            new Piece(51, "p"),
+            new Piece(52, "p"),
+            new Piece(53, "p"),
+            new Piece(54, "p"),
+            new Piece(55, "p"),
+            // 16-32 WHITE
+            new Piece(0, "R"),
+            new Piece(1, "N"),
+            new Piece(2, "B"),
+            new Piece(3, "Q"),
+            new Piece(4, "K"),
+            new Piece(5, "B"),
+            new Piece(6, "N"),
+            new Piece(7, "R"),
+            new Piece(8, "P"),
+            new Piece(9, "P"),
+            new Piece(10, "P"),
+            new Piece(11, "P"),
+            new Piece(12, "P"),
+            new Piece(13, "P"),
+            new Piece(14, "P"),
+            new Piece(15, "P"),
+
+    };
 
 }
